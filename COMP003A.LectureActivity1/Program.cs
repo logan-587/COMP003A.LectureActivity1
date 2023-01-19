f71fab7b2a376c1bada5737201e39352bbb72f69
logansmith <logansmith@LEM231-04>
2023-01-18 18:56:57 -0800

    Add project files.

---
+++ COMP003A.LectureActivity1/Program.cs
@@ -0,0 +1,18 @@
+﻿// this is a single-line comment
+/* this is a multi-line comment
+ * Author: Logan Smith
+ * Course: COMP-003A
+ * Purpose: Lecture activity for week 1
+  */
+namespace COMP003A.LectureActivity1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!"); //add comments here too
+            Console.WriteLine("this is a string");
+            Console.WriteLine("Logan Smith");
+        }
+    }
+}
